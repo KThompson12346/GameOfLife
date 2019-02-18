a98094879593933e487b92e4b8c2c51162b8f61f
KThompson <kt3223@gmail.com>
2019-02-18 15:35:57 +0000

    drawGrid() function
    function to draw the grid on the screen is written

--- Javascript/js/index.js
+++ Javascript/js/index.js
@@ -19,7 +19,7 @@ $(function(event) {
       for (var rows = 0; rows < GRIDHEIGHT; rows++) { //goes across the rows within the grid
         for (var cols = 0; cols < GRIDWIDTH; cols++) { //goes across the columns within the grid
             var cell = Math.floor(Math.Random() * 2 ) //the cells of the grid is either a 1 or 0 (alive or dead), chosen at random
-          if (cell === 1) { //if the cell variable is the same as the integer value 1 and same type int then the element at gameGrid[rows][cols] is set to a 1 else a 0, this is done at randomly.
+            if (cell === 1) { //if the cell variable is the same as the integer value 1 and same type int then the element at gameGrid[rows][cols] is set to a 1 else a 0, this is done randomly.
             gameGrid[rows][cols] = 1;
           }
           else {
@@ -28,5 +28,23 @@ $(function(event) {
         }
       }
     }
+
+    //Will draw the game grid on the screen including cells
+    function drawGrid() {
+      var gameCanvas = $("#gameCanvas"); //gets the gameCanvas element
+      var ctx = $("#gameCanvas")[0].getContext("2d");
+      ctx.clearRect(0, 0, 100, 100);
+      //both loops go through the rows and columns respectively and draws the pixel in the grid in the specified colour
+      for (var rows = 1; rows < GRIDHEIGHT; rows++) {
+        for (var cols = 1; cols < GRIDWIDTH; cols++) {
+          if (gameGrid[rows][cols] === 1) { //if the element is a 1 the pixel is coloured to represent it is alive
+            context.fillStyle = “#FF0000”;
+            context.fillRect(rows, cols, 1, 1);
+          }
+        }
+      }
+    }
+
+    
 
 })
