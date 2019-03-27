11ff9bf141cd68fd72731fa8fa9505763bb8900b
KThompson <kt3223@gmail.com>
2019-03-27 13:58:26 +0000

    create_rect() function made
    function to create the points for each rectangle is created and working

--- Python/game_of_life.py
+++ Python/game_of_life.py
@@ -3,12 +3,25 @@ from random import *
 
 window = Tk()
 window.title("John Conway's Game Of Life")
-canvas = Canvas(window, background='white', width=800, height=600)
+canvas = Canvas(window, background='white', width=500, height=500)
 
+
+def x_value():
+    return choice(range(0, 500, 10))
+def y_value():
+    return choice(range(0, 500, 10))
+
+
+def create_rect():
+    x1 = x_value()
+    y1 = y_value()
+    x2 = x1 + 10
+    y2 = y1 + 10
+    return x1, y1, x2, y2
 
 def create_grid(canvas):
-    width = 800 # specifies width of the canvas
-    height = 600 # specifies height of the canvas
+    width = 500 # specifies width of the canvas
+    height = 500 # specifies height of the canvas
 
     for line in range(0, width, 10): # range(start, stop, step)
         canvas.create_line([(line, 0), (line, height)], fill='black', tags='grid_line_w')
@@ -16,7 +29,13 @@ def create_grid(canvas):
     for line in range(0, height, 10):
         canvas.create_line([(0, line), (width, line)], fill='black', tags='grid_line_h')
 
+def draw(canvas):
+    pass
+
+def update_grid(canvas):
+    pass
 
 create_grid(canvas)
+draw(canvas)
 canvas.grid(row=0, column=0)
 window.mainloop()
