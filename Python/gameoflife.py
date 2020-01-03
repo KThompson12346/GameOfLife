c3d10007d9b11bba7094382b04163a1baa658bb7
KThompson <kt3223@gmail.com>
2020-01-03 19:49:05 +0000

    window is displaying

--- Python/gameoflife.py
+++ Python/gameoflife.py
@@ -1,7 +1,8 @@
-import tkinter
+from tkinter import *
 import random
 import time
 
 window = Tk()
-window.title('Game Of Life Python') 
+window.title('Game Of Life Python')
 game_title = Frame(window) # frame for the game of life title
+window.mainloop()
