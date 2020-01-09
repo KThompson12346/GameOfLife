72baae52d0cfb825cda54c68cba89b798966d24d
KThompson <kt3223@gmail.com>
2020-01-09 14:26:26 +0000

    added class game_window
    created a class which holds the game window i.e. game title, start button and canvas frame for drawing.

--- Python/gameoflife.py
+++ Python/gameoflife.py
@@ -2,20 +2,23 @@ from tkinter import *
 import random
 import time
 
+
+class game_window:
+
+    def __init__(self, master):
+        canvas_frame = Frame(master)
+        game_title = Frame(master)
+        start_button = Button(master, text='Start Game')
+        canvas = Canvas(canvas_frame, width=910, height=700, background='black')
+        game_title_label = Label(game_title, text='Game Of Life', font='Helvetica 20 bold', fg='grey')
+        canvas.grid(row=0, column=0)
+        canvas_frame.grid(row=1, column=1)
+        game_title_label.grid(rowspan=2, column=0)
+        game_title.grid(row=0, columnspan=2)
+        start_button.grid(rowspan=2, column=1)
+
+
 window = Tk()
 window.title('Game Of Life Python')
-game_title = Frame(window) # frame for the game of life title
-
-canvas_frame = Frame(window) # frame for the canvas
-canvas = Canvas(canvas_frame, width=910, height=700, background='black') # creates the canvas size
-
-game_title_label = Label(game_title, text='Game Of Life', font='Helvetica 20 bold', fg='grey') # creates the label for the game title
-
-canvas.grid(row=0, column=0) # puts canvas onto the canvas_frame
-canvas_frame.grid(row=1, column=1) # puts canvas_frame onto the window
-game_title_label.grid(rowspan=2, column=0) # puts the game_title_label onto the game_title frame
-game_title.grid(row=0, columnspan=2)
-
-
-
+gameoflife = game_window(window)
 window.mainloop()
