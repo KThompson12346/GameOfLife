ef64dc605ea2dff7f44ac665f752b0a1d37beb1d
KThompson <kt3223@gmail.com>
2020-05-12 21:57:29 +0100

    updated to animate not working
    animation not working program is freezing

--- Javascript/gameoflife.py
+++ Javascript/gameoflife.py
@@ -7,23 +7,7 @@ PIXEL_SIZE = 10
 ROW = 910
 COLUMN = 700
 grid = []
-
-window = Tk() # creates the window for the game
-window.title('Game Of Life Python') # is the game title written on the window
-
-canvas_frame = Frame(window) # creates a frame on the window to hold the canvas
-game_title = Frame(window) # creates a frame on the window to display the game title (which will be a label)
-start_button = Button(window, text='Start Game') # creates a button which will be used to start the game
-canvas = Canvas(canvas_frame, width=ROW, height=COLUMN, background='black') # creates the canvas used to the draw the game of life
-game_title_label = Label(game_title, text='Game Of Life', font='Helvetica 20 bold', fg='grey') # creates the label for the game title which will be placed in a frame
-
-canvas.grid(row=0, column=0) # places the canvas onto the canvas_frame
-canvas_frame.grid(row=1, column=1) # places the canvas_frame onto the window
-game_title_label.grid(rowspan=2, column=0) # places the title of the game onto the game_title frame
-game_title.grid(row=0, columnspan=2) # places the frame for the game title onto the window
-start_button.grid(rowspan=2, column=1) # places the start onto the window
-
-
+updated_grid = [[]]
 
 def create_grid():
     for row in range(0, ROW):
@@ -31,7 +15,6 @@ def create_grid():
         for column in range(0, COLUMN):
             grid2.append(randint(0, 1))
         grid.append(grid2)
-
 
 
 def draw_grid():
@@ -78,11 +61,25 @@ def apply_rules():
                     grid[row][column] = 1
                 else:
                     grid[row][column] = 0
+    window.after(0, apply_rules)
 
 
+window = Tk() # creates the window for the game
+window.title('Game Of Life Python') # is the game title written on the window
+
+canvas_frame = Frame(window) # creates a frame on the window to hold the canvas
+game_title = Frame(window) # creates a frame on the window to display the game title (which will be a label)
+start_button = Button(window, text='Start Game', command=apply_rules) # creates a button which will be used to start the game
+canvas = Canvas(canvas_frame, width=ROW, height=COLUMN, background='black') # creates the canvas used to the draw the game of life
+game_title_label = Label(game_title, text='Game Of Life', font='Helvetica 20 bold', fg='grey') # creates the label for the game title which will be placed in a frame
+
+canvas.grid(row=0, column=0) # places the canvas onto the canvas_frame
+canvas_frame.grid(row=1, column=1) # places the canvas_frame onto the window
+game_title_label.grid(rowspan=2, column=0) # places the title of the game onto the game_title frame
+game_title.grid(row=0, columnspan=2) # places the frame for the game title onto the window
+start_button.grid(rowspan=2, column=1) # places the start onto the window
 
 
 create_grid()
 draw_grid()
-apply_rules()
 window.mainloop()
