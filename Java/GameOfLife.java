f40c7f13a1b9c806fdcec307da9b69408c91b18d
KThompson <kt3223@gmail.com>
2020-06-05 20:23:42 +0100

    drawCells class working correctly
    cells are now drawn on the screen correctly, moving onto the rules of the game of life.

--- Java/GameOfLife.java
+++ Java/GameOfLife.java
@@ -10,38 +10,51 @@ import java.awt.Color;
 public class GameOfLife extends JFrame {
 
   int PIXELSIZE = 10;
-  int ROW = 700;
-  int COLUMN = 700;
+  int ROW = 70;
+  int COLUMN = 70;
   Random random = new Random();
-  int grid[][] = new int[ROW][COLUMN];
   int updatedGrid[][] = new int[ROW][COLUMN];
 
   public GameOfLife() {
+    DrawCells cellsComp = new DrawCells();
     this.setTitle("Game Of Life: Java");
-    this.setSize(ROW, COLUMN);
+    this.setSize(PIXELSIZE * ROW, PIXELSIZE * COLUMN);
     this.setLocationRelativeTo(null);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.add(cellsComp);
     this.setVisible(true);
   }
 
-  public int[][] createGrid() {
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[i].length; j++) {
-        grid[i][j] = random.nextInt(2);
+  private class DrawCells extends JComponent {
+    int x;
+    int y;
+    int grid[][] = new int[ROW][COLUMN];
+
+    public void createGrid() {
+      for (int i = 0; i < grid.length; i++) {
+        for (int j = 0; j < grid[i].length; j++) {
+          grid[i][j] = random.nextInt(2);
+        }
+      }
+      //System.out.println(Arrays.deepToString(grid).replace("], ", "]\n"));
+    }
+
+    public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      createGrid();
+      Graphics2D g2 = (Graphics2D) g;
+      for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COLUMN; j++) {
+          if (grid[i][j] == 1) {
+            g2.setColor(Color.RED);
+            g2.fillRect(i * PIXELSIZE, j * PIXELSIZE, PIXELSIZE, PIXELSIZE);
+          }
+        }
       }
     }
-    //System.out.println(Arrays.deepToString(grid).replace("], ", "]\n"));
-    return grid;
   }
 
   public static void main(String[] args) {
     GameOfLife gol = new GameOfLife();
-  }
-}
-
-public class DrawCells extends JComponent {
-  public void paintComponent(Graphics g) {
-    Graphics2D g2 = (Graphics2D) g;
-
   }
 }
